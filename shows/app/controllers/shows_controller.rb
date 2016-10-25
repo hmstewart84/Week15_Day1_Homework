082a8ae0e2299a94eeead624575703c9eb1f4c04
Hannah Stewart <hmstewart84@gmail.com>
2016-10-25 08:56:16 +0100

    no idea why it's not working

---
+++ shows/app/controllers/shows_controller.rb
@@ -0,0 +1,25 @@
+class ShowsController < ApplicationController
+
+  def index
+      @shows = Show.all
+      render :json => @shows
+  end
+
+  # def show
+  #  @show = Show.find(params[:id])
+  #  render :json => @show
+  # end
+
+  def create
+   show = Show.create( show_params )
+   render :json => show
+  end 
+
+
+  # private
+  # def show_params
+  #  params.require(:show).permit([:title, :series, :description, :image, :programmeID])
+  # end
+
+
+end
